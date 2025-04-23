da5a26ff64416f4fa0a282f20b644b1dcb5d92c2
bald123 <bouseksouyanis@gmail.com>
2025-04-23 15:29:57 +0200

    Setup of the basic multithreading to handle communication with multiple client

--- server/main.c
+++ server/main.c
@@ -9,14 +9,62 @@
 #include <arpa/inet.h>
 
 
+// dont forget about too many conn and dos!!
+
+
+char api[7] = "command";
 int socketfd;
 struct sockaddr_in addr;
 struct sockaddr_in client_addr;
+typedef struct infoclient {
+  int client;
+  char ip[INET_ADDRSTRLEN];
+} infoclient;
+
+
+void *handleClient(void *arg){
+  infoclient *client = (infoclient*)  arg;
+  printf("id: %d - from: %s - Connexion established and is being handled !\n", client->client, client->ip);
+  while (1) {
+    // Check if con is alive
+    char msg[64] = "check if conn is alive\n";
+    if (write(client->client, msg, sizeof(msg)) != sizeof(msg)){
+      printf("conn is dead\n");
+      break;
+    }
+    char buffer[512];
+    if (recv(client->client, buffer, sizeof(buffer), 0) != -1) {
+      printf("id: %d - from: %s - Message received is '%s' \n",client->client, client->ip, buffer);
+      if (strncmp(buffer, api, 7) == 0 ) {
+        printf("id: %d - from: %s - Successfull connexion to the command api ! \n", client->client, client->ip);
+      }
+      else {
+        printf("id: %d - from: %s - Connexion to the server without api args, exit!!\n", client->client, client->ip);
+        break;
+      }
+
+      
+    }
+
+  }
+
+
+  
+  // char msg[64] = "Successfull connexion, goodbye now!\n";
+  // if (write(client->, msg, sizeof(msg)) == sizeof(msg)){
+  //   printf("Success, message has been correctly send\n");
+  // }
+  // else {
+  //   printf("Error - Return does not match the size of the message\n");
+  // }
 
 
 
-void acceptCon(){
-  printf("Entering thread loop for accepting connexion !\n");
+  close(client->client);
+  free(client);
+
+  
+  return NULL;
 }
 
 int main(){
@@ -38,17 +86,18 @@ int main(){
     exit(-1);
   }
 
+  printf("Socket binded and listening !\nWaiting for new connexion...\n");
   while (1) {
-    printf("Waiting for new conn !\n");
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
     int newsocket = accept(socketfd, (struct sockaddr *) &client_addr, &client_len);
-    int *client = malloc(sizeof(newsocket));
-    *client = newsocket;
-    char ip[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &client_addr.sin_addr, ip, sizeof(ip));
-    printf("from: %s : New connexion established !\n", ip);
-
+    infoclient *client  = malloc(sizeof(infoclient));
+    client->client = newsocket; 
+    inet_ntop(AF_INET, &client_addr.sin_addr, client->ip , sizeof(client->ip));
+    // printf("id: %d - from: %s - New connexion established !\n",client->client ,client->ip);
+    pthread_t newthread;
+    pthread_create(&newthread, NULL, handleClient, client);
+    pthread_detach(newthread);
 
   //   // char msg[64] = "Successfull connexion, goodbye now!\n";
   //   if (write(newsocket, msg, sizeof(msg)) == sizeof(msg)){
