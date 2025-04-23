7b11cfd1e3017cf5ca817d001652b4b9fdbad6fd
bald123 <bouseksouyanis@gmail.com>
2025-04-23 13:57:00 +0200

    Update of main.c for the server, accept cons and display IP

--- server/main.c
+++ server/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <time.h>
+#include <arpa/inet.h>
 
 
 int socketfd;
 struct sockaddr_in addr;
+struct sockaddr_in client_addr;
+
+
+
+void acceptCon(){
+  printf("Entering thread loop for accepting connexion !\n");
+}
 
 int main(){
   printf("Starting the server program\n");
@@ -29,18 +38,26 @@ int main(){
     exit(-1);
   }
 
-
   while (1) {
     printf("Waiting for new conn !\n");
-    int newsocket = accept(socketfd, NULL, NULL);
-    char msg[64] = "Successfull connexion, goodbye now!\n";
-    if (write(newsocket, msg, sizeof(msg)) == sizeof(msg)){
-      printf("Success, message has been correctly send\n");
-    }
-    else {
-      printf("Error - Return does not match the size of the message\n");
-    }
-    close(newsocket);
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    int newsocket = accept(socketfd, (struct sockaddr *) &client_addr, &client_len);
+    int *client = malloc(sizeof(newsocket));
+    *client = newsocket;
+    char ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_addr.sin_addr, ip, sizeof(ip));
+    printf("from: %s : New connexion established !\n", ip);
+
+
+  //   // char msg[64] = "Successfull connexion, goodbye now!\n";
+  //   if (write(newsocket, msg, sizeof(msg)) == sizeof(msg)){
+  //     printf("Success, message has been correctly send\n");
+  //   }
+  //   else {
+  //     printf("Error - Return does not match the size of the message\n");
+  //   }
+  //   close(newsocket);
   }
 
 }
